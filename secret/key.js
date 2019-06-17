3486bebad1a999c03ebb4972ddba1783af1cbc1d
Hansanghyeon <dandan9509@gmail.com>
2019-06-17 18:00:35 +0900

    key file repo

---
+++ secret/key.js
@@ -0,0 +1,18 @@
+/**************************
+
+    대체하고 삭제영역
+
+    // S3
+    __BUCKET_NAME__
+    __ACCESSKEYID__
+    __SECRETACCESSKEY__
+
+***************************/
+
+module.exports = {
+	BUCKET : {
+		NAME : '__BUCKET_NAME__',
+		ACCESSKEYID : '__ACCESSKEYID__',
+		SECRETACCESSKEY : '__SECRETACCESSKEY__'
+	}
+}
