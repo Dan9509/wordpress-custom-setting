f74328509f4cd23479d0f82871b338a49bed3a3e
sanghyeon <dandan9509@gmail.com>
2019-09-12 20:53:57 +0900

    DEV: Babel, TypeScript 기능분리

---
+++ Babel/TEST.js
@@ -0,0 +1,5 @@
+let TEST = () => {
+  console.log('TEST COMPLELE!!');
+};
+
+TEST();
--- gulpfile.js
+++ gulpfile.js
@@ -5,74 +5,37 @@
 
  ***************************/
 
-// gulpfile.js
-const gulp = require("gulp");
-
 // gulp plugin
-const sourcemaps = require("gulp-sourcemaps"),
-  bb = require("gulp-babel"),
-  ts = require("gulp-typescript"),
+const
+  gulp = require("gulp"),
   CrossBrowser = require('./GulpFunctions/Sass').CrossBrowser,
   SassMix = require('./GulpFunctions/Sass').SassMix,
   SassSingle = require('./GulpFunctions/Sass').SassSingle,
-  S3Upload = require('./GulpFunctions/S3Upload').S3Upload,
-  SlackNotice = require('./GulpFunctions/Slack').SlackNotice,
-  NoticeContent = require('./GulpFunctions/Slack').NoticeContent;
+  Babel = require('./GulpFunctions/JsCompile').Babel,
+  TypeScript = require('./GulpFunctions/JsCompile').TypeScript;
 
-// Babel
-const babel = () => {
-  let before = gulp
-    .src("./Babel/*.js")
-    .pipe(sourcemaps.init())
-    .pipe(
-      bb().on("error", err => {
-        SlackNotice("Babel")(NoticeContent(err.message.toString(), 'ERROR! | Babel', '#fdd835'));
-        console.log(err.message.toString());
-        this.emit("end");
-      })
-    )
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("../public/js/"));
 
-  if (process.env.OPTION_S3) {
-    return S3Upload(before, "js");
-  } else {
-    return before;
-  }
-};
+// --------------- 구분선 ---------------
 
-// TypeScript
-const typescript = () => {
-  let before = gulp
-    .src("./TypeScript/*.ts")
-    .pipe(sourcemaps.init())
-    .pipe(
-      ts().on("error", err => {
-        SlackNotice("Typescript")(NoticeContent(err.message.toString(), 'ERROR! | Typescript', '#0288d1'));
-        console.log(err.message.toString());
-        this.emit("end");
-      })
-    )
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("../public/js/"));
-
-  if (process.env.OPTION_S3) {
-    return S3Upload(before, "js");
-  } else {
-    return before;
-  }
-};
 
 // watch
 gulp.task("hello", () => {
+
+  // 통합 SCSS 기능
   gulp.watch(
     "./Scss/mix/*.scss",
     gulp.series(gulp.parallel(SassMix), CrossBrowser)
   );
+
+  // 개별 SCSS 기능
   gulp.watch(
     "./Scss/single/*.scss",
     gulp.series(gulp.parallel(SassSingle), CrossBrowser)
   );
-  gulp.watch("./Babel/*.js", gulp.series(babel));
-  gulp.watch("./TypeScript/*.ts", gulp.series(typescript));
+
+  // Babel!!
+  gulp.watch("./Babel/*.js", gulp.series(Babel));
+
+  // TypeScript!!
+  gulp.watch("./TypeScript/*.ts", gulp.series(TypeScript));
 });
