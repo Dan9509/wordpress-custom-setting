589531472565c94a3a2582a55b7b2eceab2b4098
hyeon <dandan9509@gmail.com>
2019-09-17 15:04:13 +0900

    Fix: Slack Message content var setting

--- Gulp/GulpFunctions/Slack.js
+++ Gulp/GulpFunctions/Slack.js
@@ -8,51 +8,71 @@ const
 // --------------- 구분선 ---------------
 
 
-const SlackNotice = (user, channel) => {
-  let iconUrl = process.env.CDN_URL;
-  if(user !== undefined) {
-    switch(user) {
-      case 'Sass':
-        iconUrl += 'icons/sass.png';
-        break;
-      case 'Babel':
-        iconUrl += 'icons/babel.png';
-        break;
-      case 'Typescript':
-        iconUrl += 'icons/typescript.png';
-        break;
-      case 'S3':
-        iconUrl += 'icons/s3.png';
-        break;
-      case 'Gulp':
-        iconUrl += 'icons/gulp.png';
-        break;
-      case 'Node':
-        iconUrl += 'icons/Node.png';
-        break;
-      default:
-        iconUrl = '';
-        break;
-    }
+// Slack message setting
+const MessageContent = (gulpError, username) => {
+  let article = {
+    iconUrl: process.env.CDN_URL,
+    content: '```shell'+gulpError.message+'```',
+    contentTitle: `에러발생 | ${username}`,
+    messageSidebarColor: ''
+  };
+
+  switch (username) {
+    case 'SassMax':
+      article.iconUrl += 'icons/sass.png';
+      article.messageSidebarColor = '#ec407a';
+      break;
+    case 'SassMin':
+      article.iconUrl += 'icons/sass.png';
+      article.messageSidebarColor = '#ec407a';
+      break;
+    case 'Babel':
+      article.iconUrl += 'icons/babel.png';
+      article.messageSidebarColor = '#fdd835';
+      break;
+    case 'Typescript':
+      article.iconUrl += 'icons/typescript.png';
+      article.messageSidebarColor = '#0288d1';
+      break;
+    case 'S3':
+      article.iconUrl += 'icons/s3.png';
+      article.messageSidebarColor = '#d96735';
+      break;
+    case 'Gulp':
+      article.iconUrl += 'icons/gulp.png';
+      article.messageSidebarColor = '#ca514e';
+      break;
+    case 'Node':
+      article.iconUrl += 'icons/Node.png';
+      article.messageSidebarColor = '#79a270';
+      break;
+    default:
+      article.iconUrl = '';
+      break;
   }
 
-  return require("gulp-slack")({
-    url: process.env.SLACK_WEBHOOK,
-    user: user,
-    icon_url: iconUrl,
-    channel: channel !== undefined ? channel : ''
-  });
+  return article;
 };
 
+
+const SlackNotice = (username, channel, iconUrl) => (
+  require("gulp-slack")({
+    url: process.env.SLACK_WEBHOOK,
+    user: username,
+    icon_url: iconUrl,
+    channel: channel !== undefined ? channel : ''
+  })
+);
+
 // Slack Webhook message content
-const NoticeContent = (content, contentTitle, messageSidebarColor) => ([
+const NoticeContent = (Message) => ([
   {
-    text: process.env.PROJECT,
-    color: messageSidebarColor,
+    text: process.env.PROJECT+'\n---',
+    color: Message.messageSidebarColor,
     fields: [
       {
-        title: contentTitle,
-        value: content
+        title: Message.contentTitle,
+        value: Message.content
       }
     ]
   }
