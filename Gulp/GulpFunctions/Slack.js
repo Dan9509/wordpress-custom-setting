09b1316f88c1bb33000c39aa2eb6739b2dbf7801
hyeon <dandan9509@gmail.com>
2019-09-18 10:13:48 +0900

    Fix: Slack message 함수 한개로 합침

--- Gulp/GulpFunctions/Slack.js
+++ Gulp/GulpFunctions/Slack.js
@@ -14,49 +14,61 @@ const isOPTION = {
 };
 
 // Slack message setting
-const MessageContent = (gulpError, username) => {
-  let article = {
+let MessageContent = (message, messageType, username) => {
+  let isCodeMsg = messageType === 'code';
+  let ErrorContent = {
+    title: `에러발생 | ${username}`,
+    value: `\`\`\`shell ${message.message}\`\`\``
+  };
+  let S3uploadState = {
+    title: 'S3 Upload',
+    value: message
+  };
+
+  let msg = {
     iconUrl: process.env.CDN_URL,
-    content: '```shell'+gulpError.message+'```',
-    contentTitle: `에러발생 | ${username}`,
-    messageSidebarColor: ''
+    pushData: [{
+      text: process.env.PROJECT+'\n---',
+      color: '',
+      fields: [ isCodeMsg ? ErrorContent : S3uploadState ]
+    }]
   };
 
   switch (username) {
-    case 'SassMax':
-      article.iconUrl += 'icons/sass.png';
-      article.messageSidebarColor = '#ec407a';
+    case 'SassMix':
+      msg.iconUrl += 'icons/sass.png';
+      msg.pushData.color = '#ec407a';
       break;
     case 'SassMin':
-      article.iconUrl += 'icons/sass.png';
-      article.messageSidebarColor = '#ec407a';
+      msg.iconUrl += 'icons/sass.png';
+      msg.pushData.color = '#ec407a';
       break;
     case 'Babel':
-      article.iconUrl += 'icons/babel.png';
-      article.messageSidebarColor = '#fdd835';
+      msg.iconUrl += 'icons/babel.png';
+      msg.pushData.color = '#fdd835';
       break;
     case 'Typescript':
-      article.iconUrl += 'icons/typescript.png';
-      article.messageSidebarColor = '#0288d1';
+      msg.iconUrl += 'icons/typescript.png';
+      msg.pushData.color = '#0288d1';
       break;
     case 'S3':
-      article.iconUrl += 'icons/s3.png';
-      article.messageSidebarColor = '#d96735';
+      msg.iconUrl += 'icons/s3.png';
+      msg.pushData.color = '#d96735';
       break;
     case 'Gulp':
-      article.iconUrl += 'icons/gulp.png';
-      article.messageSidebarColor = '#ca514e';
+      msg.iconUrl += 'icons/gulp.png';
+      msg.pushData.color = '#ca514e';
       break;
     case 'Node':
-      article.iconUrl += 'icons/Node.png';
-      article.messageSidebarColor = '#79a270';
+      msg.iconUrl += 'icons/Node.png';
+      msg.pushData.color = '#79a270';
       break;
     default:
-      article.iconUrl = '';
+      msg.iconUrl = '';
       break;
   }
 
-  return article;
+  return msg;
 };
 
 
@@ -68,20 +80,6 @@ const SlackNotice = (username, channel, iconUrl) => (
     channel: channel !== undefined ? channel : ''
   })
 );
-
-// Slack Webhook message content
-const NoticeContent = (Message) => ([
-  {
-    text: process.env.PROJECT+'\n---',
-    color: Message.messageSidebarColor,
-    fields: [
-      {
-        title: Message.contentTitle,
-        value: Message.content
-      }
-    ]
-  }
-]);
 
 // Slack Upload function
 const UploadOptions = (username) => ({
@@ -118,8 +116,8 @@ const GulpSlack = (gulpError, username) => {
       if(isOPTION.upload)
         SlackUpload(process.env.SLACK_API_GULPBOT,UploadOptions(username));
       else if(isOPTION.message) {
-        let Message = MessageContent(gulpError, username);
-        SlackNotice(username, '', Message.iconUrl)(NoticeContent(Message));
+        let Message = MessageContent(gulpError, 'code', username);
+        SlackNotice(username, '', Message.iconUrl)(Message.pushData);
       }
     })
     // Log file delete
